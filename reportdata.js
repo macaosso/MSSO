dd2e49d657a9188d4e9cdfda799e588bff8e2366
macaosso <tungtung0191@gmail.com>
2026-09-18 17:47:01 +0800

    Update reportdata.js

--- reportdata.js
+++ reportdata.js
@@ -1,154 +1,132 @@
 const reportsData = [
     {
         title: "2025年8月1-3日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2025年8月1-3日 豪雨特別報告.pdf",
         date: "2025-08-06"
     },
     {
         title: "2025年8月4-6日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2025年8月4-6日 豪雨特別報告.pdf",
         date: "2025-08-07"
     },
     {
         title: "2025年8月14-15日",
-        category: "special",
         categoryName: "豪雨特別報告",        
         url: "Rreport/2025年8月14-15日 大豪雨特別報告.pdf",
         date: "2025-08-16"
     },
     {
         title: "2025年9月7-9日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2025年9月7-9日 豪雨特別報告.pdf",
         date: "2025-09-10"
     },
     {
         title: "2025年9月23-25日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2025年9月23-25日 豪雨特別報告.pdf",
         date: "2025-09-29"
     },
     {
         title: "2026年01月",
-        category: "monthly",
         categoryName: "氣象觀測月報",
         url: "Mreport/2026年01月氣象觀測月報.pdf",
         date: "2026-02-06"
     },
     {
         title: "2026年02月",
-        category: "monthly",
         categoryName: "氣象觀測月報",
         url: "Mreport/2026年02月氣象觀測月報.pdf",
         date: "2026-03-01"
     },
     {
         title: "2026年03月",
-        category: "monthly",
         categoryName: "氣象觀測月報",
         url: "Mreport/2026年03月氣象觀測月報.pdf",
         date: "2026-04-01"
     },
     {
         title: "2026年04月",
-        category: "monthly",
         categoryName: "氣象觀測月報",
         url: "Mreport/2026年04月氣象觀測月報.pdf",
         date: "2026-05-01"
     },
     {
         title: "2026年05月",
-        category: "monthly",
         categoryName: "氣象觀測月報",
         url: "Mreport/2026年05月氣象觀測月報.pdf",
         date: "2026-06-01"
     },
     {
         title: "2026年6月6-9日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2026年6月6-9日 豪雨特別報告.pdf",
         date: "2026-06-11"
     },
     {
         title: "2026年6月13-19日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2026年6月13-19日 豪雨特別報告.pdf",
         date: "2026-06-21"
     },
     {
         title: "2026年06月",
-        category: "monthly",
         categoryName: "氣象觀測月報",
         url: "Mreport/2026年06月氣象觀測月報.pdf",
         date: "2026-07-01"
     },
     {
         title: "2026年7月4-8日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2026年7月4-8日 豪雨特別報告.pdf",
         date: "2026-07-15"
     },
     {
         title: "2026年7月15-17日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2026年7月15-17日 豪雨特別報告.pdf",
         date: "2026-07-18"
     },
     {
         title: "2026年7月20-21日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2026年7月20-21日 豪雨特別報告.pdf",
         date: "2026-07-22"
     },
     {
         title: "2026年7月26-28日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2026年7月26-28日 豪雨特別報告.pdf",
         date: "2026-07-28"
     },
     {
         title: "2026年7月29-30日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2026年7月29-30日 豪雨特別報告.pdf",
         date: "2026-07-30"
     },
     {
         title: "2026年7月31日-8月2日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2026年7月31日-8月2日 豪雨特別報告.pdf",
         date: "2026-08-04"
     },
     {
         title: "2026年07月",
-        category: "monthly",
         categoryName: "氣象觀測月報",
         url: "Mreport/2026年07月氣象觀測月報.pdf",
         date: "2026-08-01"
     },
     {
         title: "2026年8月2-3日",
-        category: "special",
         categoryName: "豪雨特別報告",
         url: "Rreport/2026年8月2-3日 豪雨特別報告.pdf",
         date: "2026-08-07"
     },
     {
         title: "2026年08月",
-        category: "monthly",
         categoryName: "氣象觀測月報",
         url: "Mreport/2026年08月氣象觀測月報.pdf",
         date: "2026-09-05"
